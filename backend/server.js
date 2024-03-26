59a920b2efdb37fd55dee94ba2d0c8b382246ce3
Rohith Gottiparthi <rohith.gottiparthi@gmail.com>
2024-03-26 09:28:19 -0400

    removed production/development from server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -32,17 +31,12 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+);
 
 app.use(notFound);
 app.use(errorHandler);
